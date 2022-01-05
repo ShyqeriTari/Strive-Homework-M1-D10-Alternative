7d14dc277173cc7ee7e82df1f57c8592dce5831f
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-01-05 17:35:56 +0100

    moving through js exercises

--- m1d10.js
+++ m1d10.js
@@ -16,3 +16,60 @@ let x = "John"
 let y = "Doe"
 console.log(x + " <> " + y)
 
+let anObj = {
+    name: "Mary",
+    surname: "Louis",
+    email: "mary.louis@gmail.com"
+}
+
+console.log(anObj)
+
+delete anObj.email
+console.log(anObj)
+
+let anArr = []
+
+for (let i = 1; i <= 10; i++){
+    anArr.push(i.toString())
+}
+console.log(anArr)
+
+for (let i = 0; i < anArr.length; i++) {
+    console.log(anArr[i])
+}
+
+let otherArr = []
+
+for (let i = 0; i < 100;i++) {
+    otherArr.push(Math.floor(Math.random() * 100))
+}
+
+console.log(otherArr)
+
+let min = 101
+let max = 0
+function maxAndMin() {
+    for(let i = 0; i < otherArr.length; i++){
+        if (otherArr[i] > max) {
+           max = otherArr[i]
+        
+    } else if (otherArr[i] < min ){
+min = otherArr[i]
+    }
+}
+
+    console.log( max, min)
+}
+
+maxAndMin()
+
+let arrOfArr = [[],[],[]]
+
+for (let j = 1; j <=10; j++){
+for (let i = 0; i < arrOfArr.length; i++) {
+    arrOfArr[i].push(Math.floor(Math.random() * 10))
+}
+}
+
+console.log(arrOfArr)
+
